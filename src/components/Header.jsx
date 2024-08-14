5ef5d4a76bf377317a50f0d93ed2a799fc4e1d0c
Arushi Sharma <arushi.bhopal@gmail.com>
2024-08-14 23:12:29 +0530

    notekeeping

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+
+function Header() {
+  return (
+    <header>
+  
+      <h1>
+      <AutoStoriesIcon/>
+      Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
